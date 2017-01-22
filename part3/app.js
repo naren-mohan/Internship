daea85feb85f22d27cd042c9161ef5face272173
naren-mohan <naren.mohan.1997@gmail.com>
2017-01-22 18:54:24 +0530

    HuResTiC Hiring Challenge
    CodeBlocks IDE was used for fourth problem and Notepad++ and Mozilla
    Firefox were used for first three problems.

---
+++ part3/app.js
@@ -0,0 +1,6 @@
+function display(){
+	var x = document.forms["loginform"]["username"].value;
+	var y = document.forms["loginform"]["pwd"].value;
+	console.log("Username:",x);
+	console.log("Password:",y);
+	}
